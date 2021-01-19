f189a9ebeecf21db7eb006e8d0328e767fd8ffa0
Marc Harding <info@marcharding.de>
2021-01-19 16:54:07 +0100

    Default composer version 1.

--- migraw.sh
+++ migraw.sh
@@ -616,7 +616,9 @@ echo "$DELEGATES" >> $BIN/adminer/index.php
 
     # composer
     mkdir -p $BIN/composer/
-    wget -q -O $BIN/composer/composer.phar https://getcomposer.org/composer-stable.phar
+    wget -q -O $BIN/composer/composer-1.phar https://getcomposer.org/composer-1.phar
+    wget -q -O $BIN/composer/composer-2.phar https://getcomposer.org/composer-2.phar
+    cp $BIN/composer/composer-1.phar $BIN/composer/composer.phar
 
     # extract files
     echo "Extracting:"
