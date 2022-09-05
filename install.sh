7de2db2905f754c7ea8d649830a59ef2be71a36b
Marc Harding <info@marcharding.de>
2022-09-05 22:15:45 +0200

    Install script fixes.

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 COLOR_CYAN='\e[0;36m'
 COLOR_NC='\e[0m'
 MIGRAW=/Users/"$USER"/migraw
-sudo mkdir -p $MIGRAW
-sudo curl -s -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/marcharding/migraw/osx/migraw.sh?$(date +%s)" --output "$MIGRAW/migraw.sh"
-sudo chmod +x $MIGRAW/migraw.sh
-sudo ln -sf $MIGRAW/migraw.sh $MIGRAW/migraw
+mkdir -p $MIGRAW
+curl -s -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/marcharding/migraw/osx/migraw.sh?$(date +%s)" --output "$MIGRAW/migraw.sh"
+chmod +x $MIGRAW/migraw.sh
+ln -sf $MIGRAW/migraw.sh $MIGRAW/migraw
 
 echo -e "\n${COLOR_CYAN}Downloaded migraw.${COLOR_NC}"
 echo -e "\nTo complete the instalation just add the following path to your \$PATH:"
