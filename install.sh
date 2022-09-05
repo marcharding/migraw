74f2bf7df45c78b0d7be11a3000f81386377c76b
Marc Harding <info@marcharding.de>
2022-09-05 22:00:36 +0200

    Fix symlink creation in install script.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ MIGRAW=/Users/"$USER"/migraw
 sudo mkdir -p $MIGRAW
 sudo curl -s -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/marcharding/migraw/osx/migraw.sh?$(date +%s)" --output "$MIGRAW/migraw.sh"
 sudo chmod +x $MIGRAW/migraw.sh
-sudo ln -rsf $MIGRAW/migraw.sh $MIGRAW/migraw
+sudo ln -sf $MIGRAW/migraw.sh $MIGRAW/migraw
 
 echo -e "\n${COLOR_CYAN}Downloaded migraw.${COLOR_NC}"
 echo -e "\nTo complete the instalation just add the following path to your \$PATH:"
